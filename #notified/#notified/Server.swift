96ad9497499cdab73f5fd3e72da21f2a867c328f
Ben Lichtman <runite623@gmail.com>
2016-02-06 00:52:16 -0500

    added functions for getting roster and admins

--- #notified/#notified/Server.swift
+++ #notified/#notified/Server.swift
@@ -10,17 +10,39 @@ import Foundation
 import Alamofire
 
 class Server {
-    static let SERVER = "http://ec1880c5.ngrok.io/"
+    static let SERVER = "http://29b4e03b.ngrok.io/"
     
     static func pingServer() {
         Alamofire.request(.GET, SERVER).responseString(completionHandler: {
             response in
             print(response.request)  // original URL request
             print(response.response) // URL response
-            var dataString = NSString(data: response.data!, encoding:NSUTF8StringEncoding)
+            let dataString = NSString(data: response.data!, encoding:NSUTF8StringEncoding)
             print(dataString!)
             print(response.result)   // result of response serialization
 
+        })
+    }
+    
+    static func roster () {
+        Alamofire.request(.GET, SERVER + "roster").responseString(completionHandler: {
+            response in
+            print(response.request)  // original URL request
+            print(response.response) // URL response
+            let dataString = NSString(data: response.data!, encoding:NSUTF8StringEncoding)
+            print(dataString!)
+            print(response.result)   // result of response serialization
+        })
+    }
+    
+    static func admins () {
+        Alamofire.request(.GET, SERVER + "admins").responseString(completionHandler: {
+            response in
+            print(response.request)  // original URL request
+            print(response.response) // URL response
+            let dataString = NSString(data: response.data!, encoding:NSUTF8StringEncoding)
+            print(dataString!)
+            print(response.result)   // result of response serialization
         })
     }
 }
