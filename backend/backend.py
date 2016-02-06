bd6ebc544f66c6c75d2a4d5414f8619957fa2333
Alex Frieder <alex.frieder@gmail.com>
2016-02-06 01:27:53 -0500

    Fully implemented backend with Python db

--- backend/backend.py
+++ backend/backend.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask.ext.api import status
 from apns import APNs, Payload
 from time import strftime
 
@@ -17,7 +18,7 @@ afrieder = {
   'long': None,
   'admin': None,
   'checkedin': False,
-  'token': "Uhhhhhhh..?"
+  'token': "Uhhhhhhh..?",
 }
 User[afrieder['id']] = afrieder
 
@@ -29,55 +30,68 @@ blichtma = {
   'long': None,
   'admin': True,
   'checkedin': False,
-  'token': "Uhhhhhhh..?"
+  'token': "Uhhhhhhh..?",
 }
 User[blichtma['id']] = blichtma
 
+def get_admins():
+  return filter(lambda user: user['admin'], User.values())
 
 def notify_admins(msg):
-  return 5
+  admins = get_admins()
+  for admin in admins:
+    payload = Payload(alert=msg, sound="default", badge=1)
+    apns.gateway_server.send_notification(admin['token'], payload)
 
 def new_report(sender):
-  id = num_reports
+  global num_reports
+  report_id = num_reports
   report = {
     'time': strftime('%I:%M %p'),
-    'id': id,
+    'id': report_id,
     'sender': sender
   }
-  Report[id] = report
+  Report[report_id] = report
   num_reports += 1
-  return id
+  return report_id
 
 @app.route('/checkin', methods=['POST'])
 def checkin():
-  id = request.form['id']
-  User[id]['lat'] = request.form['lat']
-  User[id]['lat'] = request.form['long']
-  User[id]['checkedin'] = True
+  user_id = request.form['id']
+  User[user_id]['lat'] = request.form['lat']
+  User[user_id]['long'] = request.form['long']
+  User[user_id]['checkedin'] = True
+  return ''
 
 @app.route('/checkout', methods=['POST'])
 def checkout():
-  User[request.form['id']]['checkedin'] = False
+  user_id = request.form['id']
+  User[user_id]['checkedin'] = False
+  User[user_id]['lat'] = None
+  User[user_id]['long'] = None
+  return ''
 
 @app.route('/report', methods=['POST'])
 def report():
-  id = request.form['id']
-  User[id]['lat'] = request.form['lat']
-  User[id]['lat'] = request.form['long']
-  return jsonify({'id': new_report(id if request.form['anon'] else None)})
+  user_id = request.form['id']
+  User[user_id]['lat'] = request.form['lat']
+  User[user_id]['long'] = request.form['long']
+  return jsonify({'id': new_report(user_id if not request.form['anon'] else None)})
 
 @app.route('/lowbatt', methods=['POST'])
 def lowbatt():
-  id = request.form['id']
-  User[id]['lat'] = request.form['lat']
-  User[id]['lat'] = request.form['long']
-  notify_admins("{} has a low battery.".format(User[id]['name']))
+  user_id = request.form['id']
+  User[user_id]['lat'] = request.form['lat']
+  User[user_id]['long'] = request.form['long']
+  notify_admins("{} has a low battery.".format(User[user_id]['name']))
+  return ''
 
 @app.route('/reportinfo', methods=['POST'])
 def reportinfo():
-  id = request.form['id']
-  Report[id]['data'] = request.form['data']
-  notify_admins("The report opened at {} has been updated.".format(Report[id]['time']))
+  user_id = request.form['id']
+  Report[user_id]['data'] = request.form['data']
+  notify_admins("The report opened at {} has been updated.".format(Report[user_id]['time']))
+  return ''
 
 @app.route('/roster', methods=['GET'])
 def roster():
@@ -85,39 +99,54 @@ def roster():
   users = map(lambda user: {key: user[key] if key in user else None for key in keys}, User.values())
   return jsonify({'roster': users})
 
+@app.route('/checkedin', methods=['GET'])
+def checkedin():
+  keys = ['name', 'phone', 'lat', 'long', 'id']
+  users = filter(lambda user: user['checkedin'], User.values())
+  users = map(lambda user: {key: user[key] if key in user else None for key in keys}, users)
+  return jsonify({'roster': users})
+
 @app.route('/admins', methods=['GET'])
 def admins():
   keys = ['name', 'phone', 'id']
-  admin_users = filter(lambda user: user['admin'], User.values())
-  admin_dicts = map(lambda user: {key: user[key] if key in user else None for key in keys}, admin_users)
-  return jsonify({'admins': admin_dicts})
+  admin = map(lambda user: {key: user[key] if key in user else None for key in keys}, get_admins())
+  return jsonify({'admins': admin})
 
-@app.route('/requests', methods=['GET'])
+@app.route('/reports', methods=['GET'])
 def reports():
-  keys = ['id', 'sender', 'handler', 'lat', 'long', 'data']
+  keys = ['id', 'sender', 'handler', 'lat', 'long', 'data', 'time']
   report_list = map(lambda report: {key: report[key] if key in report else None for key in keys}, Report.values())
-  [report.update('name': "Report opened at {}".format(report['time'])) for report in report_list]
+  [report.update(name="Report opened at {}".format(report['time'])) for report in report_list]
   return jsonify({'reports': report_list})
 
 @app.route('/addadmin', methods=['POST'])
 def addadmin():
   User[request.form['id']]['admin'] = True
+  return ''
 
 @app.route('/deladmin', methods=['POST'])
 def deladmin():
+  if len(get_admins()) == 1:
+    return 'You are the only admin. You cannot delete yourself.', status.HTTP_406_NOT_ACCEPTABLE
   User[request.form['id']]['admin'] = None
+  return ''
 
 @app.route('/toggleduty', methods=['POST'])
 def toggleduty():
+  if len(get_admins()) == 1:
+    return 'You are the only admin. You cannot go off-duty.', status.HTTP_406_NOT_ACCEPTABLE
   User[request.form['id']]['admin'] = not User[request.form['id']]['admin']
+  return ''
 
 @app.route('/handlereport', methods=['POST'])
 def handlereport():
   Report[request.form['id']]['handler'] = request.form['handler']
+  return ''
 
 @app.route('/clearreport', methods=['POST'])
 def clearreport():
   del Report[request.form['id']]
+  return ''
 
 if __name__ == '__main__':
     app.run(debug=True)
